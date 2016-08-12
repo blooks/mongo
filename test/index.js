d5b66851f35a1ac4758565ba0b961c19a2261be7
Levin Keller <post@levinkeller.de>
2016-08-12 13:50:21 +0200

    Improving various stuff, moving over to blooks.

--- test/index.js
+++ test/index.js
@@ -1,1 +1,17 @@
-return;
+var Mongo = require('../index')
+
+var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/blooks'
+
+describe('@blooks/mongo tests', () => {
+  describe('Start stop tests', () => {
+    it('should start and stop the mongo db connection', (done) => {
+      var mongo = new Mongo(MONGO_URL)
+      mongo.start((err) => {
+        if (err) {
+          return done(err)
+        }
+        mongo.stop(done)
+      })
+    })
+  })
+})
